247d1890e554957b9b37f9ef23e8c1d7d3bc4e6f
John Hardy <jh@lagado.com>
2022-01-20 23:24:28 +1100

    save

---
+++ test.co.mac.asm
@@ -0,0 +1,8 @@
+.macro coDefs
+    DB ":T $ \\#1 \\#1 ;"     ; val --    2tor 2>r
+    DB ":F \\#2 \\#2 $ ;"     ; -- val    2rfrom 2r>
+
+    DB ":Y F $ T ;"
+    DB ":P 1_( 100+ Q Y ) ;"        ; endless
+    DB ":C \$ 0 P 1_( Q.\\$ 1000 > \_ Y ) ' B ;"
+.endm
